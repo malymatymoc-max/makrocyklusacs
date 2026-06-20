a2a5880d654b94493a857b15fa98983c8e84c8c9
malymatymoc-max <malymatymoc@gmail.com>
2026-06-20 17:39:10 +0200

    Bump service worker after Vercel deploy config fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "makrocyklus-mvp-v15";
+const CACHE_NAME = "makrocyklus-mvp-v16";
 const ASSETS = ["./", "./index.html", "./styles.css", "./config.js", "./app.js", "./progress-fix.js", "./phase-model.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
