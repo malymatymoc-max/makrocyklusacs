84d93892df5c63e9ba21c1aa3749a2948ce5661e
malymatymoc-max <malymatymoc@gmail.com>
2026-06-20 16:47:53 +0200

    Cache Supabase config asset

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "makrocyklus-mvp-v11";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "makrocyklus-mvp-v12";
+const ASSETS = ["./", "./index.html", "./styles.css", "./config.js", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
